3bcb4d03e82d325f4bc66fb55af9608f222bac8f
DjThunder <contact@b3dgs.com>
2013-12-11 20:00:43 +0100

    Audio update.

--- src/com/b3dgs/warcraft/Music.java
+++ src/com/b3dgs/warcraft/Music.java
@@ -17,8 +17,8 @@
  */
 package com.b3dgs.warcraft;
 
-import com.b3dgs.lionengine.audio.AudioMidi;
 import com.b3dgs.lionengine.audio.Midi;
+import com.b3dgs.lionengine.core.AudioMidi;
 import com.b3dgs.lionengine.core.Media;
 import com.b3dgs.lionengine.core.UtilityMedia;
 
